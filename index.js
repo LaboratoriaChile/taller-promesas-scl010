5d8822f32a600bcddbe928cc8be0101c3f40930d
Niennis <estefania.osses.v@gmail.com>
2019-08-28 21:59:11 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const fetch = require('fetch');
+const fetchUrl = fetch.fetchUrl;
+const fs = require('fs');
+
+let myFirstPromise = Promise.resolve('Hola mundo');
+
+myFirstPromise.then(response => {
+    console.log('UNO', response)
+});
+
+let mySecondPromise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve(5), 200);
+})
+
+// mySecondPromise.then(res => {
+//     res+=9;
+//     console.log('DOS', res);
+// });
+
+
+// fetchUrl a un sitio
+const getData = (url) => {
+    return new Promise ((resolve, reject) => {
+        fetchUrl(url, (error, meta, body) => {
+            if(meta){
+                if(meta.status == 200) {
+                    resolve(meta.status.toString());
+                }
+            } else {
+                reject(error);
+            }
+        })
+    })
+}
+
+let url = 'https://www.google.com/';
+
+// getData(url)
+//     .then(res => {
+//         console.log('TRES: El estado de', url, 'es: ', res);
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     })
+
+
+const readFile = (fileName, type) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(fileName, type, (error, content) =>{
+            if(error) {
+                reject(error);
+            } else {
+                resolve(content);
+            }
+        })
+    })
+}
+
+
+// readFile('./prueba/hola.md', 'utf-8')
+//     .then(res => {
+//         console.log('CUATRO', res)
+//     })
+//     .catch(err => {
+//         console.log(err)
+//     })
+
+    // Promise.all([myFirstPromise, mySecondPromise, getData(url), readFile('./prueba/hola.md', 'utf-8')])
+    //     .then(response => {
+    //         console.log(response[1]+=9)
+    //     })
